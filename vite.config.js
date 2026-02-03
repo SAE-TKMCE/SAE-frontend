f3bb9b9cf18a543e9fb5485264d4af4d2e73d441
itshabeeb <habeebmniyas04@gmail.com>
2026-02-04 01:19:06 +0530

    beta test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      'Content-Security-Policy': "default-src 'self'; img-src 'self' data: https: http: blob:; media-src 'self' data: https: http: blob: https://bmtbljnbdtzdmswopfcp.supabase.co; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; font-src 'self' data: https:; connect-src 'self' https: http: wss:;",
+      'Content-Security-Policy': "default-src 'self' https://*.supabase.co; img-src 'self' data: https: http: blob:; media-src 'self' data: https: http: blob: https://*.supabase.co; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; font-src 'self' data: https:; connect-src 'self' https: http: wss:;",
     },
   },
 });
