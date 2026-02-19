8206d03821b0f7db31a5343a5d249488a649821a
itshabeeb <habeebmniyas04@gmail.com>
2026-02-19 20:29:09 +0530

    api fix

--- src/services/certificates.js
+++ src/services/certificates.js
@@ -2,7 +2,7 @@ import api from './api';
 
 export const certificatesService = {
   async verifyCertificate(email, pin) {
-    const response = await api.post('/certificates/verify/', { email, pin });
+    const response = await api.post('api/certificates/verify/', { email, pin });
     return response.data;
   },
 };
